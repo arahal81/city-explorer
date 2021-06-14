8b19be1ae29722385ca1d5b9dd7fb8fbac0620cb
Ali Rahhal <arahal81@gmail.com>
2021-06-14 23:13:30 +0300

    lab-07

---
+++ src/components/FormS.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react'
+import { Form, Col, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+class FormS extends Component {
+  render() {
+    return (
+      <>
+        <div className="gap"></div>
+        <Form onSubmit={this.props.getCity}>
+          <Form.Row className="align-items-center">
+            <Col xs="auto">
+              <Form.Control className="mb-2" placeholder="Enter City Name" name='City_Name'/>
+            </Col>
+            <Col xs="auto">
+              <Button  type="submit" variant="dark">Search</Button>
+            </Col>
+          </Form.Row>
+        </Form>
+        <div className="gap"></div>
+      </>
+    )
+  }
+}
+export default FormS
---
+++ src/components/Weather.js
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col } from 'react-bootstrap/';
+import WeatherCards from './WeatherCards';
+export class Weather extends React.Component {
+    render() {
+        return (
+           
+                    <>
+                        <div className="gap"></div>
+                        <h1>Weather for 16 days</h1>
+                        <Row className="justify-content-md-center">
+                        {this.props.weatherInfo.map((data,i) => (
+                            <Col md="auto">
+                            <WeatherCards
+                              key={i}
+                              date={data.date}
+                              description={data.description}
+                            />
+                          </Col>
+                        ))}
+                      </Row>
+                      <div className="gap"></div>
+                    </>
+                  )
+                }
+              }
+              
+export default Weather
